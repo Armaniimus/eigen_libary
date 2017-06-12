8f02c2b82959780941130efcc4a2869b1d6e059d
Armand <517226@edu.rocmn.nl>
2017-06-12 13:19:49 +0200

    added jQuery stappen plan

---
+++ current_libary/jquery plan/JQuery stappenplan.js
@@ -0,0 +1,17 @@
+//1. create files
+    //js.js
+    //index.html
+
+//2. build basic html
+    //add 3 li elements with thing inside of them
+    //add 1 a elements with an id named 'hi'
+    //add 1 a elements with an class named 'class'
+
+//3 link the js.js file
+    //add to html --> <script src="js.js"></script>
+    //add to js.js -> alert('hi')
+    //load index.html in the browser
+
+//4. link the cdn
+    // google jQuery CDN
+    // add the top most jQuery named 3x jQuery
