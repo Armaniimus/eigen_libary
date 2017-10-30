f0264c223e014b793a57a44d6578fea825a59168
Armand_laptop <517226@edu.rocmn.nl>
2017-10-30 21:14:42 +0100

    added javascript for the list checklist and white color is now fully supported

---
+++ current_libary/Armaniimus_style_libary-v1.1/javascript/listcheckbox_controller.js
@@ -0,0 +1,12 @@
+document.querySelector("#bereidingswijze").addEventListener("click", controlRecept, event);
+document.querySelector("#ingredienten").addEventListener("click", controlRecept, event);
+function controlRecept(event) {
+    if (event.target.tagName == "LI") {
+
+        if (event.target.className == "active") {
+            event.target.className = "";
+        } else {
+            event.target.className = "active";
+        }
+    }
+}
---
+++ current_libary/Armaniimus_style_libary-v1.1/notes___linear-gradientbutton.md
@@ -0,0 +1,7 @@
+isolate the class from the class list
+test the class for starting colour
+test the class for the end colour
+
+think of a way how to format a class
+
+"button--linear-s$color-e$color"
