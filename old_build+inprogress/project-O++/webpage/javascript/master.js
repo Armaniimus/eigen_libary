ed9e27bea2ae6e2601a9a83bddccde56ec5fbaaa
Armand_laptop <517226@edu.rocmn.nl>
2017-11-03 20:09:29 +0100

    added dome selected component

--- old_build+inprogress/project-O++/webpage/javascript/master.js
+++ old_build+inprogress/project-O++/webpage/javascript/master.js
@@ -23,22 +23,56 @@ function addDomIcons() {
     }
 }
 
-// controls the switch of class and icon so the domtree can collapse and expand
+/*====================
+ DOM control functions
+======================*/
 function controlDomTree(e) {
     if (e.target.classList.contains('dom-arrows') ) {
-        if (e.target.classList.contains('arrowOpen')) {
-            e.target.innerHTML = "⮚";
-        } else {
-            e.target.innerHTML = "⮛";
-        }
-
-        e.target.nextElementSibling.nextElementSibling.classList.toggle("page-box");
-        e.target.nextElementSibling.nextElementSibling.classList.toggle("page-box-hidden");
-
-        e.target.classList.toggle("arrowOpen");
-        e.target.classList.toggle("arrowClosed");
+        controlColapseAndApear(e)
+    } else if (e.target.tagName == "LI") {
+        controlSelected(e);
     }
 };
 
+
+/******************************
+ Controls appear and collapse
+ of the arrows
+******************************/
+function controlColapseAndApear(e) {
+    if (e.target.classList.contains('arrowOpen')) {
+        e.target.innerHTML = "⮚";
+    } else {
+        e.target.innerHTML = "⮛";
+    }
+
+    e.target.nextElementSibling.nextElementSibling.classList.toggle("page-box");
+    e.target.nextElementSibling.nextElementSibling.classList.toggle("page-box-hidden");
+
+    e.target.classList.toggle("arrowOpen");
+    e.target.classList.toggle("arrowClosed");
+}
+
+/**********************************
+ Controls the selected dom element
+***********************************/
+function controlSelected(e) {
+    if (e.target.classList.contains('dom-selected') ) {
+
+    } else {
+        document.querySelector('.dom-selected').classList.remove('dom-selected');
+        e.target.classList.add('dom-selected');
+    }
+};
+
+
+/*===============
+ controlAddChild
+================*/
+function controlAddChild(e) {
+
+}
+
+document.getElementById('addId').addEventListener("click", controlAddChild, 1);
 document.querySelector('.dom-navigation').addEventListener("click", controlDomTree, 1);
 addDomIcons();
