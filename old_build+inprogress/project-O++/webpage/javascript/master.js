51f655e0ac4ab5093274c3569b223cadba0bd653
Armand_laptop <517226@edu.rocmn.nl>
2017-11-04 23:29:03 +0100

    added the basis for the addchild functionality

--- old_build+inprogress/project-O++/webpage/javascript/master.js
+++ old_build+inprogress/project-O++/webpage/javascript/master.js
@@ -2,25 +2,34 @@
 function addDomIcons() {
     const openContent =  document.querySelectorAll(".dom-nav--opencontent");
     for (let i = 0; i < openContent.length; i++) {
-        const arrowOpen = document.createElement("div");
-        arrowOpen.innerHTML = "⮛";
-        arrowOpen.classList.add("dom-arrows");
-        arrowOpen.classList.add("arrowOpen");
 
-        const parent = openContent[i].parentNode;
-        parent.insertBefore(arrowOpen ,openContent[i]);
+        setOpenArrow(openContent[i])
     }
 
     const closedContent =  document.querySelectorAll(".dom-nav--closedcontent");
     for (let i = 0; i < closedContent.length; i++) {
-        const arrowClosed = document.createElement("div");
-        arrowClosed.innerHTML = "⮚";
-        arrowClosed.classList.add("dom-arrows");
-        arrowClosed.classList.add("arrowClosed");
-
-        const parent = closedContent[i].parentNode;
-        parent.insertBefore(arrowClosed ,closedContent[i]);
+        setClosedArrow(closedContent[i])
     }
+}
+
+function setOpenArrow(element) {
+    const arrowOpen = document.createElement("div");
+    arrowOpen.innerHTML = "⮛";
+    arrowOpen.classList.add("dom-arrows");
+    arrowOpen.classList.add("arrowOpen");
+
+    const parent = element.parentNode;
+    parent.insertBefore(arrowOpen ,element);
+}
+
+function setClosedArrow(element) {
+    const arrowClosed = document.createElement("div");
+    arrowClosed.innerHTML = "⮚";
+    arrowClosed.classList.add("dom-arrows");
+    arrowClosed.classList.add("arrowClosed");
+
+    const parent = element.parentNode;
+    parent.insertBefore(arrowClosed ,element);
 }
 
 /*====================
@@ -63,16 +72,99 @@ function controlSelected(e) {
         document.querySelector('.dom-selected').classList.remove('dom-selected');
         e.target.classList.add('dom-selected');
     }
-};
-
+}
 
 /*===============
  controlAddChild
 ================*/
-function controlAddChild(e) {
+function controlAddChild() {
+    const currentElementType = document.querySelector('.dom-selected').innerHTML;
+
+    //createElement
+    const inputFormSuround = document.createElement('div');
+    const inputForm = addChildAddSelect(currentElementType);
+
+
+    //addclasses
+    inputFormSuround.classList.add('inputformsurounding');
+    inputForm.classList.add('inputform');
+
+
+    //append everything
+    inputFormSuround.appendChild(inputForm);
+    document.querySelector('body').prepend(inputFormSuround);
+
+    //add a event listener
+    document.getElementById('addchildbutton').addEventListener("click", procesAddChild);
+}
+
+function addChildAddSelect(type) {
+    const inputField = document.createElement('div');
+
+    let selectElementType = '<select class="form-addelement-select" id="addelement-type">';
+    if (type == 'Header') {
+        const HeaderOptions = ['Div','Ul','H1','H2', 'H3'];
+
+        for (let i = 0; i < HeaderOptions.length; i++) {
+             selectElementType += "<option>" + HeaderOptions[i] + "</option>";
+        }
+    }
+    selectElementType += '</select>';
+
+    inputField.innerHTML += '<label class="form-addelement-label" for="addelement-type">Specify element type</label>';
+    inputField.innerHTML += selectElementType;
+
+    inputField.innerHTML += '<button class="form-addelement-button" id="addchildbutton">Add child</button>';
+
+    return inputField;
+}
+
+function procesAddChild() {
+
+    // get all neccesary information
+    const getElementType = document.getElementById('addelement-type');
+    const selectedElement = document.querySelector('.dom-selected');
+    const newElementTagName = getElementType.value;
+    const parent = selectedElement.nextElementSibling;
+
+    // create the new element
+    const newElement = document.createElement('LI');
+    newElement.classList.add("dom-nav--content")
+    newElement.innerHTML = newElementTagName;
+
+    if (parent.tagName == 'UL') {
+        alert('yes');
+    } else {
+        // get neccesary information
+        const container = parent.parentNode;
+
+        // create container
+        const newElementContainer = document.createElement('ul');
+        newElementContainer.classList.add("dom-indent");
+        newElementContainer.classList.add("page-box");
+
+        // append element to new container
+        newElementContainer.appendChild(newElement);
+
+        // append container
+        container.insertBefore(newElementContainer, parent);
+
+    }
+
+    // removes addChildForm
+    const remove = document.querySelector('.inputformsurounding');
+    remove.parentNode.removeChild(remove);
+
+    // Set the arrows and classes for the controlling element
+    selectedElement.classList.remove('dom-nav--content');
+    selectedElement.classList.add('dom-nav--opencontent');
+
+    setOpenArrow(selectedElement)
 
 }
 
-document.getElementById('addId').addEventListener("click", controlAddChild, 1);
+
+
+document.getElementById('addChild').addEventListener("click", controlAddChild)
 document.querySelector('.dom-navigation').addEventListener("click", controlDomTree, 1);
 addDomIcons();
