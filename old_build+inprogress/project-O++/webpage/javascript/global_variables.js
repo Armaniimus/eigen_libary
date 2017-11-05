655e3f0127d1a495f005938a3a377fc935f6cf7e
Armand_laptop <517226@edu.rocmn.nl>
2017-11-06 00:17:04 +0100

    quickbackup api functions

--- old_build+inprogress/project-O++/webpage/javascript/global_variables.js
+++ old_build+inprogress/project-O++/webpage/javascript/global_variables.js
@@ -33,7 +33,7 @@ function HTMLglobal__addAttribute(attributeName, value) {
 
 }
 
-function HTMLglobal__search(searchStr) {
+function HTMLglobal__search(searchStr, func, name, attributeValue) {
     searchStr = searchStr.split("/");
     let strCount = 0;
     let savePath = [];
@@ -75,7 +75,6 @@ function HTMLglobal__search(searchStr) {
         // when there are mutlible elements set test data
         let strSplit = searchStr[strCount].split('-');
         if (strSplit.length > 1) {
-            console.log(strSplit);
             searchStr[strCount] = strSplit[0];
             multibleElement.active = 1;
             multibleElement.countTo = strSplit[1];
@@ -116,6 +115,7 @@ function HTMLglobal__search(searchStr) {
                     returns.strCount = strCount + 1;
                     returns.returnObject = object.children[i];
 
+                    console.log(object.children[i])
                     //send return object
                     return returns;
                 }
@@ -123,7 +123,43 @@ function HTMLglobal__search(searchStr) {
         }
         return false;
     }
-    return savePath;
+    function subFunc__traveseGlobal() {
+        if (savePath.length == 1) {
+            HTMLglobal.children[savePath[0]];
+
+        } else if (savePath.length == 2) {
+            HTMLglobal.children[savePath[0]].children[savePath[1]];
+
+        } else if (savePath.length == 3) {
+            HTMLglobal.children[savePath[0]].children[savePath[1]].children[savePath[2]];
+
+        } else if (savePath.length == 4) {
+            HTMLglobal.children[savePath[0]].children[savePath[1]].children[savePath[2]].children[savePath[3]];
+        }
+
+
+
+
+    }
+
+    function subFunc__mutate() {
+        if (func == 'addchild' || func == 0) {
+            object = Htmlglobal__createChild(name);
+            return true;
+
+        } else if (func == 'addclass' || func == 1) {
+            HTMLglobal__addClass(name);
+            return true;
+
+        } else if (func == 'addattribute' || func == 2) {
+            HTMLglobal__addAttribute(name, attributeValue);
+            return true;
+
+        } else if (func == 'addid' || func == 3) {
+            HTMLglobal__addId(name);
+            return true;
+        }
+    }
 }
 
 function testdivs() {
@@ -138,9 +174,11 @@ function testdivs() {
 
 testdivs()
 
+console.log(HTMLglobal__search('html/body/header/div-4', 0, 'div'))
+
 // console.log(HTMLglobal__search('html-body-header') );
-console.log(HTMLglobal__search('html/body/header/div-4') );
+//console.log(HTMLglobal__GetChildUrl('html/body/header/div-4') );
 
 
 
-console.log(HTMLglobal);
+// console.log(HTMLglobal);
