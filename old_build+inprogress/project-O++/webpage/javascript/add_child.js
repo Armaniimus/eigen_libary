e2490a2a09091e14440b579357805ebeca592f41
Armand_laptop <517226@edu.rocmn.nl>
2017-11-05 01:32:23 +0100

    organized the select groups

--- old_build+inprogress/project-O++/webpage/javascript/add_child.js
+++ old_build+inprogress/project-O++/webpage/javascript/add_child.js
@@ -6,7 +6,7 @@ function controlAddChild() {
 
     //createElement
     const inputFormSuround = document.createElement('div');
-    const inputForm = addChildAddSelect(currentElementType);
+    const inputForm = addChildAddform(currentElementType)
 
 
     //addclasses
@@ -22,17 +22,13 @@ function controlAddChild() {
     document.getElementById('addchildbutton').addEventListener("click", procesAddChild);
 }
 
-function addChildAddSelect(type) {
+function addChildAddform(currentElementType) {
     const inputField = document.createElement('div');
 
     let selectElementType = '<select class="form-addelement-select" id="addelement-type">';
-    if (type == 'Header') {
-        const HeaderOptions = ['Div','Ul','H1','H2', 'H3'];
 
-        for (let i = 0; i < HeaderOptions.length; i++) {
-             selectElementType += "<option>" + HeaderOptions[i] + "</option>";
-        }
-    }
+    selectElementType += addChildAddOptions(currentElementType);
+
     selectElementType += '</select>';
 
     inputField.innerHTML += '<label class="form-addelement-label" for="addelement-type">Specify element type</label>';
@@ -42,6 +38,137 @@ function addChildAddSelect(type) {
 
     return inputField;
 }
+
+function addChildAddOptions(currentElementType) {
+    let optionBodyArray;
+
+    optionBodyArray = testElementMajor(currentElementType);
+
+    if (optionBodyArray == true) {
+        testElementList(currentElementType);
+    }
+
+    if (optionBodyArray == true) {
+        testElementTable(currentElementType);
+    }
+
+    if (optionBodyArray == true) {
+        testElementForm(currentElementType);
+    }
+
+    if (optionBodyArray == true) {
+        testElementCommon(currentElementType);
+    }
+
+    if (optionBodyArray == true) {
+        alert('Element is not supported');
+    } else {
+        let selectBody;
+        for (let i = 0; i <  optionBodyArray.length; i++) {
+             selectBody += "<option>" +  optionBodyArray[i] + "</option>";
+        }
+        return selectBody;
+    }
+
+}
+
+function testElementMajor(currentElementType) {
+    if (currentElementType == 'Header') {
+         optionBodyArray = ['Div','Ul', 'Img', 'H1','H2', 'H3', 'Button', 'P'];
+
+    } else if (currentElementType == 'Main') {
+         optionBodyArray = ['Div','Ul', 'Ol', 'Table', 'Form', 'Img', 'H1','H2', 'H3', 'H4', 'H5','Aside', 'Article', 'Section', 'span', 'A', 'Button', 'P'];
+
+    } else if (currentElementType == 'Footer') {
+         optionBodyArray =  ['Div','Ul', 'Img', 'H1','H2', 'H3', 'Button', 'P'];
+
+    } else if (currentElementType == 'Section') {
+         optionBodyArray =  ['Div','Ul', 'Ol', 'Table', 'Form', 'Img', 'H1','H2', 'H3', 'H4', 'H5', 'span', 'A', 'Button', 'P', 'Article'];
+
+    } else if (currentElementType == 'Article') {
+         optionBodyArray =  ['Div','Ul', 'Ol', 'Table', 'Form', 'Img', 'H1','H2', 'H3', 'H4', 'H5', 'span', 'A', 'Button', 'P'];
+
+    } else if (currentElementType == 'Aside') {
+         optionBodyArray =  ['Div','Ul', 'Ol', 'Table', 'Img', 'H1','H2', 'H3', 'H4', 'H5', 'span', 'A', 'Button', 'P'];
+
+    } else {
+        return true;
+    }
+
+    return optionBodyArray;
+}
+
+function testElementList(currentElementType) {
+    if (currentElementType == 'Ul') {
+         optionBodyArray =  ['Li'];
+
+    } else if (currentElementType == 'Ol') {
+         optionBodyArray =  ['Li'];
+
+    } else if (currentElementType == 'Li') {
+         optionBodyArray =  ['A', 'Ul', 'Ol', 'Div', 'Img', 'Button', 'P'];
+
+    } else {
+       return 1 == 1;
+   }
+
+   return optionBodyArray;
+}
+
+function testElementTable(currentElementType) {
+    if (currentElementType == 'Table') {
+         optionBodyArray =  ['Thead', 'Tfoot', 'Tbody', 'Caption', 'Colgroup'];
+
+    } else if (currentElementType == 'Thead') {
+         optionBodyArray =  ['Tr'];
+
+    } else if (currentElementType == 'Tbody') {
+         optionBodyArray =  ['Tr'];
+
+    } else if (currentElementType == 'Tfoot') {
+         optionBodyArray =  ['Tr'];
+
+    } else if (currentElementType == 'Colgroup') {
+         optionBodyArray =  ['Col'];
+
+    } else {
+       return true;
+   }
+
+   return optionBodyArray;
+}
+
+function testElementForm(currentElementType) {
+    if (currentElementType == 'Form') {
+        optionBodyArray =  ['Input', 'Button', 'Select', 'Textarea', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'Fieldset', 'Label', 'Optgroup'];
+
+   } else if (currentElementType == 'Select') {
+        optionBodyArray =  ['Option'];
+
+   } else if (currentElementType == 'Optgroup') {
+        optionBodyArray =  ['Option'];
+
+   } else if (currentElementType == 'Fieldset') {
+        optionBodyArray =  ['Legend', 'Input', 'Textarea', 'Select', 'Label', 'Optgroup', 'Button'];
+
+   } else {
+       return true;
+   }
+
+   return optionBodyArray;
+}
+
+function testElementCommon(currentElementType) {
+    if (currentElementType == 'Div') {
+             optionBodyArray =  ['Div','Ul', 'Ol', 'Table', 'Form', 'Img', 'H1','H2', 'H3', 'H4', 'H5','Aside', 'Article', 'Section', 'A', 'Button', 'P', 'Input'];
+
+    } else {
+       return true;
+   }
+
+   return optionBodyArray;
+}
+
 
 function procesAddChild() {
 
