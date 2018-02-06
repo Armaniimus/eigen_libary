2c38f4c28ffa47596d155cd8640abc45a15207ee
Armand_laptop <517226@edu.rocmn.nl>
2018-02-06 04:04:14 +0100

    temp

--- old_build+inprogress/php/php_crud/crud_Module.php
+++ old_build+inprogress/php/php_crud/crud_Module.php
@@ -4,32 +4,46 @@
 ******************/
 if (isset($_POST['create'] ) || isset($_POST['read'] ) || isset($_POST['update'] ) || isset($_POST['delete'] ) ) {
 
-    //set the url
-    $url = $_POST['url'];
+    function setUrl() {
+        $url = $_POST['url'];
+        if (isset( $_POST['submit_update'] ) ) {
+            $url = $_POST['submit_update'];
+        }
+        $url = str_replace('..', '', $url);
 
-    // controls the create
-    if (isset($_POST['create'] ) ) {
-        $content = new crud_Module($url, $_POST['content']);
-        $createResult = $content->create();
-    }
+        return $url;
+    };
+    $url = setUrl();
 
-    // controls the read
-    if (isset($_POST['read'] ) ) {
-        $content = new crud_Module($url);
-        $readResult = $content->read();
-    }
+    function controlCrud($url) {
 
-    // controls the update
-    if (isset($_POST['update'] ) ) {
-        $content = new crud_Module($url, $_POST['content']);
-        $updateResult = $content->update();
-    }
+        // controls the create
+        if (isset($_POST['create'] ) ) {
+            $content = new crud_Module($url, $_POST['content']);
+            $crudResult = $content-> create();
+        }
 
-    // controls the delete
-    if (isset($_POST['delete'] ) ) {
-        $content = new crud_Module($url);
-        $deleteResult = $content->delete();
+        // controls the read
+        if (isset($_POST['read'] ) ) {
+            $content = new crud_Module($url);
+            $crudResult = $content-> read();
+        }
+
+        // controls the update
+        if (isset($_POST['update'] ) ) {
+            $content = new crud_Module($url, $_POST['content']);
+            $crudResult = $content-> update();
+        }
+
+        // controls the delete
+        if (isset($_POST['delete'] ) ) {
+            $content = new crud_Module($url);
+            $crudResult = $content-> delete();
+        }
+
+        return $crudResult;
     }
+    controlCrud($url);
 }
 ////
 
@@ -37,46 +51,40 @@ class crud_Module {
     private $url;
     private $content;
     private $mode;
-    public $result;
+    public $result = [];
 
     public function __construct($url, $content = null) {
-        //if update is submitted set submitted url
-        if (isset( $_POST['submit_update_url'] ) ) {
-            $url = $_POST['submit_update_url'];
-        }
-
-        $url = str_replace('..', '', $url);
         $this->url = $url;
         $this->content = $content;
     }
 
     public function create() {
+        $this->result["mode"] = "create";
         $myfile = fopen($this->url, "w") or die("Unable to open file!");
         fwrite($myfile, $this->content);
         fclose($myfile);
 
-        return "submit_create";
+        return $this->result;
     }
 
     public function read() {
-
+        $this->result["mode"] = "read";
         if (file_exists($this->url)) {
             $myfile = fopen($this->url, "r");
 
             // try to read the file;
             if (!(filesize($this->url) == false || filesize($this->url) == 0)) {
-                $this->result = fread($myfile, filesize($this->url));
+                $this->result["content"] = fread($myfile, filesize($this->url));
                 fclose($myfile);
             }
 
             // if size = 0 but file exists return an empty string otherwise throw error;
             else {
-                // $this->result = ">>>ERROR: No data found file is empty<<<";
-                $this->result = "";
+                $this->result["content"] = "";
             }
 
         } else {
-            $this->result = ">>>ERROR: No file found<<<";
+            $this->result["content"] = ">>>ERROR: No file found<<<";
         }
 
         return $this->result;
@@ -85,16 +93,23 @@ class crud_Module {
     public function update() {
         if (isset($_POST['submit_update'] ) ) {
             $this->create($this->url, $this->content);
-            return "submit_update";
+            $this->result["mode"] = "submit_update";
+
+            return $this->result;
 
         } else {
-            return $this->read();
+            $this->result["content"] = $this->read();
+            $this->result["mode"] = "setup_update";
+
+            return $this->result;
         }
     }
 
     public function delete() {
         unlink($this->url);
-        return "submit_delete";
+        $this->result["mode"] = "delete";
+
+        return $this->result;
     }
 }
 
