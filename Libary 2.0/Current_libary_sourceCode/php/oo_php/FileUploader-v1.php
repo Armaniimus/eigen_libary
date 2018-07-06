760ba83a370232f08291eb8ae82f20c2df588ef3
Armand_laptop <517226@edu.rocmn.nl>
2018-07-06 19:55:26 +0200

    updated fileuploader to be more secure an generic with support for png and jpg/jpeg

--- Libary 2.0/Current_libary_sourceCode/php/oo_php/FileUploader-v1.php
+++ Libary 2.0/Current_libary_sourceCode/php/oo_php/FileUploader-v1.php
@@ -1,49 +1,82 @@
 <?php
-
 // confimed support png, jpeg
-// may support but not tested svg, webp, bmp, gif,
+// may support but not tested webp, bmp, gif
+// will not support svg due to the nature of it having executeable code
 class FileUploader {
-    private $postName;
+    private $testData;
+
+    private $filesPostName;
 
     private $tmp_name;
     private $file_name;
     private $file_size;
 
-    private $file_type; // is header given by browser
-    private $file_mime; // is a check got from a method
-    private $file_extention; // is the extention formatted as file_mime and file_type
+    private $file_type;         // is header given by browser
+    private $file_mime;         // is a check got from a method
+    private $file_extention;    // is the extention formatted as file_mime and file_type
 
-    private $raw_extention; // is the raw extention
+    private $raw_extention;     // is the raw extention
+    private $NULLFileName;      // is the given filename without extention
+    private $lastUploadedFile;  // is the full url of the uploaded file
 
-    public function __construct($postName) {
+    public function __construct($filesPostName) {
+        $this->testData = 0;
+        $lastUploadedFile = NULL;
+
         // set limits of file uploads
-        // ini_set("memory_limit", "2000M"); // max memory
         ini_set("post_max_size", "30M"); // max total post size
         ini_set("upload_max_filesize", "2M"); // max size per file
-        $this->postName = $postName;
+
+        // set post variable.
+        $this->filesPostName = $filesPostName;
+
+        // set image extracted variables
+        if (count($_FILES) && !empty($_FILES["$filesPostName"]["name"]) ) {
+            $this->SetVariables();
+        }
     }
 
-    public function UploadImage($folder, $name) {
-        $postName = $this->postName;
-        $url = $folder . "/" . $name;
+    // uploads image to the folder given in the variable
+    // and gives the file the name thats given in the variable or keep the upload name of the file
+    public function UploadImage($folder, $fileName = NULL) {
+        if ($fileName === NULL) {
+            $fileName = $this->NULLFileName;
+        }
 
-        if (count($_FILES) && !empty($_FILES["$postName"]["name"]) ) {
-            $this->SetVariables();
+        $filesPostName = $this->filesPostName;
+        $localUrl = $folder . "/" . $fileName;
+
+        // Check if there is something inside of $_files and if needed variable is not empty
+        if (count($_FILES) && !empty($_FILES["$filesPostName"]["name"]) ) {
 
             if ($this->ValidateFile() ) {
                 switch ($this->file_mime) {
                     case "image/jpg":
                     case "image/jpeg":
                     case "image/png":
-                    case "image/svg":
                     case "image/webp":
                     case "image/gif":
                     case "image/bmp":
-                        $this->MoveFile($url);
+                        $this->MoveFile($localUrl);
                         break;
                 }
             }
+
+            if ($this->testData === 1) {
+                echo $this->file_type . "<br>";
+                echo $this->file_mime . "<br>";
+                echo $this->file_extention . "<br>";
+            }
         }
+        return $this->GetLastUploadedFile();
+    }
+
+    public function GetRawExtention() {
+        return $this->raw_extention;
+    }
+
+    public function GetLastUploadedFile() {
+        return $this->lastUploadedFile; //leads to the url of the file
     }
 
     private function SetVariables() {
@@ -70,14 +103,21 @@ class FileUploader {
     }
 
     private function GetExtention($file_name) {
+        $test1 = explode("/", $file_name);
+        $test2 = explode("\\", $file_name);
+
+        if (count($test1) + count($test2) > 2) {
+            $this->ThrowError("user has added a forbidden character to the filename");
+        }
+
         // Get file extention and prevent double extention
         $fileNameArray = explode(".", $file_name);
         if (count($fileNameArray) > 2) {
-            echo "<pre>";
-            throw new Exception("FileNames With more then 1 . are not allowed", 1);
-            echo "</pre>";
+            $this->ThrowError("FileNames With more then 1 . are not allowed");
+
         } else {
             $raw_extention = $fileNameArray[1];
+            $this->NULLFileName = $fileNameArray[0];
         }
 
         // convert extention to match header type and mimetype format
@@ -89,9 +129,6 @@ class FileUploader {
                 break;
             case "png":
                 $file_extention = "image/png";
-                break;
-            case "svg":
-                $file_extention = "image/svg";
                 break;
             case "webp":
                 $file_extention = "image/webp";
@@ -111,36 +148,64 @@ class FileUploader {
     }
 
     private function ValidateFile() {
+        $filesPostName = $this->filesPostName;
+
+        // dissallow upload if upload file doesn't match
+        if (!is_uploaded_file($_FILES["$filesPostName"]['tmp_name']) ) {
+            $this->ThrowError("user tried to upload a file but file name didn't match the actual upload");
+            return 0;
+        }
 
         // disallow upload if reports of the browser header, extention or mime type do not match.
-        if ($this->file_type !== $this->file_mime) {
-            echo "<pre>";
+        else if ($this->file_type !== $this->file_mime) {
+            $this->ThrowError("File Validation Fails\n \$file_type !== \$file_mime \n");
+            return 0;
 
-            throw new Exception("File Validation Fails
-            \n \$file_type !== \$file_mime \n", 1);
-
-            echo "</pre>";
-
+        //  disallow upload if mimetype does not match the file extention
         } else if ($this->file_mime !== $this->file_extention) {
-            echo "<pre>";
 
-            throw new Exception("File Validation Fails
+            $message = "File Validation Fails
             \n \$file_mime !== \$file_extention
             \n \$file_mime => $this->file_mime
-            \n \$file_extention => $this->file_extention", 1);
+            \n \$file_extention => $this->file_extention";
 
-            echo "</pre>";
-        } else {
+            $this->ThrowError("$message");
+            return 0;
+
+        // allow upload if there are no <?php tags or <script tags in the file>
+        } else if ($this->InDeptValidation() ) {
+            return 1;
+        }
+    }
+
+    private function InDeptValidation() {
+        $content            =   file_get_contents($this->tmp_name);
+        $screenedContent    =   file_get_contents($this->tmp_name);
+
+        // Scan for Html and script tags
+        $screenedContent    =   str_replace("script","","$screenedContent");
+        $screenedContent    =   str_replace("<?php","","$screenedContent");
+
+        // if there are no php or script tags found return true
+        if ($screenedContent === $content) {
+            unset($content);
+            unset($screenedContent);
             return 1;
         }
     }
 
     private function MoveFile($url) {
-        if(!move_uploaded_file($this->tmp_name, "$url.$this->raw_extention")){
-            echo "<pre>";
-            throw new Exception("Error uploading file", 1);
-            echo "</pre>";
+        $this->lastUploadedFile = "$url.$this->raw_extention";
+
+        if(!move_uploaded_file($this->tmp_name, $this->lastUploadedFile) ) {
+            $this->ThrowError("Error uploading file");
         }
+    }
+
+    private function ThrowError($message) {
+        echo "<pre>";
+        throw new Exception("$message", 1);
+        echo "</pre>";
     }
 }
 ?>
