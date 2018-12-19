a642548a414ede8bdbbf2c18ddcfcdd84b4442a6
Armand_laptop <517226@edu.rocmn.nl>
2018-12-19 13:27:50 +0100

    reorganized configFile

--- mvc_scaffolding/Router/config.php
+++ mvc_scaffolding/Router/config.php
@@ -2,10 +2,9 @@
 // Define("BESTAND_DIEPTE", 3);
 Define("APP_DIR", "/php/eigen_libary/mvc_scaffolding");
 
-
-Define("DB_NAME", "moviesRus");
+Define("DB_HOST", "localhost");
+Define("DB_NAME", "tennisRus");
 Define("DB_USERNAME", "root");
 Define("DB_PASS", "");
-Define("DB_SERVER_ADRESS", "localhost");
 Define("DB_TYPE", "mysql");
 ?>
