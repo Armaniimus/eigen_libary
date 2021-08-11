9c26bcddadc61830051a62f3439438a92de875f8
Armaniimus <>
2021-08-12 01:18:06 +0200

    added sanitation options

--- web/php_components/Sanitizer.php
+++ web/php_components/Sanitizer.php
@@ -37,6 +37,15 @@ class Sanitizer {
     }
 
     /**
+     * filters invalid chars from mail;
+     * @param  [type] $value [description]
+     * @return [type]        [description]
+     */
+    public static function url($value) {
+        return filter_var($value, FILTER_SANITIZE_URL);
+    }
+
+    /**
      * filters invalid chars from input;
      * @param  [type] $value [description]
      * @return [type]        [description]
@@ -61,6 +70,69 @@ class Sanitizer {
      */
     public static function decode_specialChars($value) {
         return htmlspecialchars_decode( trim($value) );
+    }
+
+    /**
+     * sanitizes a date
+     * @param  [type] $value [description]
+     * @return [type]        [description]
+     */
+    public static  function date($value) {
+        $value = self::text($value);
+
+        try {
+            $array = explode('-', $value);
+            $year = $array[0];
+            $month = $array[1];
+            $day = $array[2];
+
+            if(!checkdate($month, $day, $year)) {
+                return $value;
+            }
+        }
+
+        //runs if an error occurred or no valid date is found;
+        return '0000-00-00';
+    }
+
+    /**
+     * sanitizes a time
+     * @param  [type] $value [description]
+     * @return [type]        [description]
+     */
+    public static function time($value) {
+        $value = self::text($value);
+
+        try {
+            $array = explode(':', $value);
+            if (count($array) == 2) {
+                $hour = (int) $array[0];
+                $min  = (int) $array[1];
+
+                if ($hour >= 0 && $hour <= 23) {
+                    if ($min >= 0 && $min <= 59) {
+                        return $value;
+                    }
+                }
+            }
+        }
+
+        //runs if an error occurred or no valid date is found;
+        return '0:00';
+    }
+
+    /**
+     * sanitizes a hexadecimal
+     * @param  [type] $value [description]
+     * @return [type]        [description]
+     */
+    public static function hexadecimal($value)  {
+        $value = self::text($value);
+        if ( preg_match('/^#?([a-f0-9]{3}|[a-f0-9]{6})$/') ) {
+            return $value;
+        }
+
+        return '#000000';
     }
 }
 ?>
